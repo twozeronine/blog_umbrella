b752ae269e67483cd300f25ecc240e178a3ee329
twozeronine <amdusias209@gmail.com>
2022-10-17 23:27:10 +0900

    feat : user api 수정

--- apps/blog_api/lib/blog_api/controllers/user_controller.ex
+++ apps/blog_api/lib/blog_api/controllers/user_controller.ex
@@ -1,11 +1,23 @@
 defmodule BlogApi.UserController do
   use BlogApi, :controller
 
-  def create(conn, params) do
-    render(conn, "new.json")
+  def index(conn, _params) do
+    # users = Accounts.user_list
+    # render(conn, "index.json", users: users)
+    render(conn, "index.json",
+      users: [%{user_name: "1", user_email: "2"}, %{user_name: "1", user_email: "2"}]
+    )
   end
 
-  def new(conn, params) do
-    render(conn, "new.json")
-  end
+  # def create(conn, params) do
+  #   case Accounts.create_user(params) do
+  #     {:ok, user} ->
+  #       conn
+  #       # |> 로그인 로직
+  #       |> render("index.json")
+
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "index.json")
+  #   end
+  # end
 end
--- apps/blog_api/lib/blog_api/views/user_view.ex
+++ apps/blog_api/lib/blog_api/views/user_view.ex
@@ -1,7 +1,14 @@
 defmodule BlogApi.UserView do
   use BlogApi, :view
 
-  def render("new.json", data) do
-    %{}
+  def render("index.json", %{users: users}) do
+    render_many(users, __MODULE__, "user.json")
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      user_name: user.user_name,
+      user_email: user.user_email
+    }
   end
 end
