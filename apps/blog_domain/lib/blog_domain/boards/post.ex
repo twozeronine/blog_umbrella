2f7253f9a32a2f170514f1c1c1eac7d6ff7bbf75
twozeronine <amdusias209@gmail.com>
2022-10-17 12:39:51 +0900

    feat : posts migration

---
+++ apps/blog_domain/lib/blog_domain/boards/post.ex
@@ -0,0 +1,22 @@
+defmodule BlogDomain.Boards.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @table "posts"
+
+  schema @table do
+    field(:title, :string)
+    field(:description, :string)
+
+    belongs_to(:user, BlogDomain.Accounts.User)
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :description])
+    |> cast_assoc(:user)
+    |> validate_required([:title])
+  end
+end
---
+++ apps/blog_domain/priv/repo/migrations/20221017030846_create_posts.exs
@@ -0,0 +1,17 @@
+defmodule BlogDomain.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  @table "posts"
+
+  def change do
+    create table(@table) do
+      add(:title, :string, [{:null, false}])
+      add(:description, :text)
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:posts, :user_id)
+  end
+end
