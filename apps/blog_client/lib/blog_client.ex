6af06bdf68830e791621976947645937405ab576
twozeronine <amdusias209@gmail.com>
2022-10-17 22:45:52 +0900

    feat : user_api 구현

--- apps/blog_client/lib/blog_client.ex
+++ apps/blog_client/lib/blog_client.ex
@@ -1,18 +1,43 @@
 defmodule BlogClient do
-  @moduledoc """
-  Documentation for `BlogClient`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def get_user(user_id) do
+    case HTTPoison.get("http://localhost:4000/api/users/#{user_id}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> IO.puts(body)
+      {:ok, %HTTPoison.Response{status_code: 400}} -> IO.puts("Not found")
+      {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect(reason)
+    end
+  end
 
-  ## Examples
+  def register_user({user_name, user_email, password}) do
+    req_body = Jason.encode!(%{user_name: user_name, user_email: user_email, password: password})
 
-      iex> BlogClient.hello()
-      :world
+    case HTTPoison.post(
+           "http://localhost:4000/api/users/",
+           req_body,
+           [{"Content-Type", "application/json"}]
+         ) do
+      {:ok, %HTTPoison.Response{body: body, token: token}} ->
+        GenServer.cast(self(), {:token, token})
 
-  """
-  def hello do
-    :world
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect(reason)
+    end
+  end
+
+  def start_link(_init_arg) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  @impl true
+  def init(init_arg) do
+    HTTPoison.start()
+
+    {:ok, init_arg}
+  end
+
+  @impl true
+  def handle_cast({:token, token}, state) do
+    {:noreply, Map.put(state, :token, token)}
   end
 end
