ec98ef789603881877da8c44644ee6decba02826
twozeronine <amdusias209@gmail.com>
2022-10-18 17:10:45 +0900

    refactor : 주석 제거

--- apps/blog_api/lib/blog_api.ex
+++ apps/blog_api/lib/blog_api.ex
@@ -1,22 +1,4 @@
 defmodule BlogApi do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use BlogApi, :controller
-      use BlogApi, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
-
   def controller do
     quote do
       use Phoenix.Controller, namespace: BlogApi
@@ -32,11 +14,9 @@ defmodule BlogApi do
         root: "lib/blog_api/templates",
         namespace: BlogApi
 
-      # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
-      # Include shared imports and aliases for views
       unquote(view_helpers())
     end
   end
@@ -58,7 +38,6 @@ defmodule BlogApi do
 
   defp view_helpers do
     quote do
-      # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
 
       import BlogApi.ErrorHelpers
@@ -66,9 +45,6 @@ defmodule BlogApi do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
--- apps/blog_api/mix.exs
+++ apps/blog_api/mix.exs
@@ -18,9 +18,6 @@ defmodule BlogApi.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {BlogApi.Application, []},
@@ -28,13 +25,9 @@ defmodule BlogApi.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:blog, in_umbrella: true},
@@ -49,9 +42,6 @@ defmodule BlogApi.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get"],
