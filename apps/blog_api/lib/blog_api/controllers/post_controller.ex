f9dc0ff448f53ae03cf41272e7ffd7327db43057
twozeronine <amdusias209@gmail.com>
2022-10-19 20:40:20 +0900

    feat : post router controller 구현

--- apps/blog_api/lib/blog_api/controllers/post_controller.ex
+++ apps/blog_api/lib/blog_api/controllers/post_controller.ex
@@ -1,50 +1,55 @@
 defmodule BlogApi.PostController do
   use BlogApi, :controller
 
-  # alias BlogDomain.Boards
-  # alias BlogDomain.Boards.Post
+  alias BlogApi.Utils
+  alias BlogDomain.Boards
+  alias BlogDomain.Boards.Post
+  alias BlogDomain.Accounts
 
-  # def index(conn, _params) do
-  #   posts = Boards.post_list()
-  #   render(conn, "index.json", posts)
-  # end
+  def index(conn, _params) do
+    posts = Boards.post_list()
+    render(conn, "index.json", %{posts: posts})
+  end
 
-  # def show(conn, %{"id" => id}) do
-  #   post = Boards.get_post(id)
-  #   render(conn, "show.json", %{post: post})
-  # end
+  def show(conn, %{"id" => id}) do
+    post = Boards.get_post(id)
+    render(conn, "show.json", %{post: post})
+  end
 
-  # def create(conn, %{"user" => user, "post" => post_params}) do
-  #   case Post.create_post(user, post_params) do
-  #     {:ok, %Post{} = post} ->
-  #       conn
+  def create(conn, %{"user" => %{"id" => id}, "post" => post_params}) do
+    user = Accounts.get_user(id)
 
-  #     # |> put_status(:created)
-  #     # |> put_resp_header("location", Routes.post_path(conn, :show, post))
-  #     # |> render("show.json", post)
+    case Boards.create_post(user, post_params) do
+      {:ok, %Post{} = post} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", Routes.post_path(conn, :show, post))
+        |> render("show.json", %{post: post})
 
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       :error
-  #   end
-  # end
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn |> render("errors.json", %{errors: Utils.format_changeset_errors(changeset)})
+    end
+  end
 
-  # def update(conn, %{"user" => user, "post" => post_params}) do
-  #   case Boards.update_user_post(user, post_params.post_id, params) do
-  #     {:ok, {:ok, %Post{} = post}} ->
-  #       render(conn, "show.json", %{post: post})
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    case Boards.update_post(id, post_params) do
+      {:ok, {:ok, %Post{} = post}} ->
+        render(conn, "show.json", %{post: post})
 
-  #     {:ok, {:error, %Ecto.Changeset{} = changeset}} ->
-  #       :error
+      {:ok, {:error, %Ecto.Changeset{} = changeset}} ->
+        conn |> render("errors.json", %{errors: Utils.format_changeset_errors(changeset)})
 
-  #     {:error, _} ->
-  #       :error
-  #   end
-  # end
+      {:error, _} ->
+        conn |> render("errors.json", %{errors: Utils.internal_server_error()})
+    end
+  end
 
-  # def delete(conn, %{"id" => id}) do
-  #   case Boards.delete_post(post) do
-  #     {:ok, %Post{}} -> send_resp(conn, :no_content, "")
-  #     _ -> :error
-  #   end
-  # end
+  def delete(conn, %{"id" => id}) do
+    post = Boards.get_post(id)
+
+    case Boards.delete_post(post) do
+      {:ok, %Post{}} -> send_resp(conn, :no_content, "")
+      _ -> conn |> render("errors.json", %{errors: Utils.internal_server_error()})
+    end
+  end
 end
