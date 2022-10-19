a0565f4b9cbbf1340cd729fd2c0aba9edfdf8281
twozeronine <amdusias209@gmail.com>
2022-10-19 22:16:44 +0900

    feat : 잘못된 업데이트 케이스 수정

--- apps/blog_domain/lib/blog_domain/accounts.ex
+++ apps/blog_domain/lib/blog_domain/accounts.ex
@@ -12,10 +12,10 @@ defmodule BlogDomain.Accounts do
 
   def update_user(%User{id: id}, params \\ %{}) do
     fn ->
-      id
-      |> get_user_lock()
-      |> User.changeset(params)
-      |> Repo.update()
+      case get_user_lock(id) do
+        %User{} = user -> User.changeset(user, params) |> Repo.update()
+        nil -> {:error, :not_found}
+      end
     end
     |> Repo.transaction()
   end
