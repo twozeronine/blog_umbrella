1198038ba688438c5f9a9e7b3d5ff92efdf874c3
twozeronine <amdusias209@gmail.com>
2022-10-24 17:36:47 +0900

    feat : invalid test user token

--- apps/blog_api/test/blog_api/controllers/user_controller_test.exs
+++ apps/blog_api/test/blog_api/controllers/user_controller_test.exs
@@ -38,6 +38,27 @@ defmodule BlogApi.UserControllerTest do
            } == json_response(conn, 200)
   end
 
+  test "GET /users  invalid authentication", %{conn: conn} do
+    %User{id: user_id} =
+      Enum.map(1..5, fn num ->
+        user_fixture(%{
+          user_name: @valid_params.user_name <> "#{num}",
+          user_email: @valid_params.user_email <> "#{num}",
+          password: @valid_params.password <> "#{num}"
+        })
+      end)
+      |> hd()
+
+    assert invalid_token = BlogApi.Token.generate_and_sign_token(user_id - 0)
+
+    conn =
+      conn
+      |> put_req_header("authorization", invalid_token)
+      |> get(Routes.user_path(conn, :index))
+
+    refute conn.halted
+  end
+
   test "GET /users/:id", %{conn: conn} do
     %User{id: user_id} = user_fixture(@valid_params)
 
