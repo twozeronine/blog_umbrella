fc015c51a004c5ef9db49b96b217bb9bccbd5aec
twozeronine <amdusias209@gmail.com>
2022-10-31 16:07:24 +0900

    feat : 피닉스 버전 다운그레이드

--- apps/blog_web/mix.exs
+++ apps/blog_web/mix.exs
@@ -35,7 +35,7 @@ defmodule BlogWeb.MixProject do
       {:phoenix, "~> 1.6.12"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18"},
+      {:phoenix_live_view, "~> 0.17.12", override: true},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
