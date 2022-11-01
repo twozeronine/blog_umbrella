0df6cf99742f2adc6966b12891a011c348dadc38
twozeronine <amdusias209@gmail.com>
2022-11-01 15:31:50 +0900

    feat : error 메세지 나오도록 수정

--- apps/blog_web/lib/blog_web/views/error_helpers.ex
+++ apps/blog_web/lib/blog_web/views/error_helpers.ex
@@ -2,7 +2,7 @@ defmodule BlogWeb.ErrorHelpers do
   use Phoenix.HTML
 
   def error_tag(form, field) do
-    Enum.map(Keyword.get_values(form.errors, field), fn error ->
+    Enum.map(Keyword.get_values(form.source.errors, field), fn error ->
       content_tag(:span, translate_error(error),
         class: "invalid-feedback",
         phx_feedback_for: input_name(form, field)
