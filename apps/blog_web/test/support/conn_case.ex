8f6fc6d3a8991db418746f1c661b3673ebb8325a
twozeronine <amdusias209@gmail.com>
2022-11-05 22:22:30 +0900

    feat : mix test 버그 수정

--- apps/blog_web/test/support/conn_case.ex
+++ apps/blog_web/test/support/conn_case.ex
@@ -14,7 +14,8 @@ defmodule BlogWeb.ConnCase do
     end
   end
 
-  setup _tags do
+  setup tags do
+    BlogDomain.DataCase.setup_sandbox(tags)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
